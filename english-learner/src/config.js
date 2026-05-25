d77884e5f2a4167946b0578e01427522039b7327
Boban02 <boban.lazarov@hotmail.com>
2026-05-26 00:11:56 +0200

    Initial commit

--- english-learner/src/config.js
+++ english-learner/src/config.js
@@ -1,4 +1,6 @@
 // App configuration
 const CONFIG = {
-
+    PROVIDER: 'groq',
+    GROQ_API_KEY: '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',
+    GOOGLE_API_KEY: 'AIzaSyBbQaQpIglLXNr8-rMW5sfTLy1JlIeQcY4'
 };
